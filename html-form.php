47b1d04351ec3beeba98efbbabe208d9b966b79c
VedranIteh <58921122+VedranIteh@users.noreply.github.com>
2020-04-05 03:53:20 +0200

    Update html-form.php

--- html-form.php
+++ html-form.php
@@ -20,14 +20,13 @@ $_SESSION['when'] = rand(10,99).date("U"); //timestamp and some random nums to m
 <input class="fields" type="text" placeholder="Phone number" name="number" id="contact-number"> /* good field */
 <input class="fields" type="text" placeholder="city" name="city" id="contact-city"> /* honeypot 1 */
 <input class="fields" type="text" placeholder="county" name="county" id="contact-county"> /* honeypot 2 */
-<!-- <input class="fields" type="text" placeholder="state" name="state" id="contact-state"> -->  /* honeypot 3 - disabled for css reasons, uncomment here and in css to work or just delete it all*/
 <input type="hidden" placeholder="zip" name="zip" id="contact-zip"> /* honeypot 4 */ 
 <input class="fields" type="text" placeholder="Surname" name="surname" id="contact-surname"> /* honeypot 5 */ 
 <input class="fields" type="text" placeholder="City" name="city2" id="contact-city2"> /* good field */
 <input class="fields" type="text" placeholder="County" name="county2" id="contact-county2"> /* good field */ 
 <input class="fields" type="text" placeholder="State" name="state2" id="contact-state2"> /* good field */
-<input type="hidden" placeholder="<?php echo $_SESSION['when']; ?>" name="control" id="control"> /* dont remove*/
-<input type="textarea" name="message" id="message"> /* good field */
+<input type="hidden" value="<?php echo $_SESSION['when']; ?>" name="control" id="control"> /* dont remove !!! */
+<textarea name="message" id="message"></textarea> /* good field */
 </form>
 <script>
 var h5 = document.getElementById("contact-surname");
